28516afa44b5420b180ff034e1309e7fe973610d
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-11-21 23:28:50 -0300

    Exercicio Corrigido

--- Exercicio 3/exe3.js
+++ Exercicio 3/exe3.js
@@ -1,7 +1,7 @@
-let nomeContribuente = prompt("Informe o nome do vendedor do expediente ?")
-let codigoDaPeca = Number(prompt("Informe o código da peça"))
-let precoPeca = Number(prompt("Informe o preço da peça"))
-let quantidadeVendida = Number(prompt("Informe a quantidade vendida ?"))
+let nomeContribuente = prompt("Informe o nome do vendedor(a) do expediente?")
+let codigoDaPeca = Number(prompt("Informe o código da peça?"))
+let precoPeca = Number(prompt("Informe o preço da peça?"))
+let quantidadeVendida = Number(prompt("Informe a quantidade vendida?"))
 
 let totalVenda = quantidadeVendida * precoPeca
 
@@ -12,8 +12,8 @@ function comissaoCincoPorcento(){
   return resultado
 }
 
-console.log("O nome do Vendedor(a) ",nomeContribuente)
-console.log("O Código da peça é ",codigoDaPeca)
-console.log( "O preço da peça está ",precoPeca)
-console.log("A quantidade que foi vendida é ",quantidadeVendida)
-console.log("O total da venda",totalVenda)
+console.log("O nome do Vendedor(a) é",nomeContribuente)
+console.log("O Código da peça é",codigoDaPeca)
+console.log( "O preço da peça está R$",precoPeca)
+console.log("A quantidade que foi vendida é",quantidadeVendida)
+console.log("O total da venda foi R$",totalVenda.toFixed(2))
