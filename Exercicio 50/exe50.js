c188d14ea280c087f8e52cda6cbcab09f1fcef75
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-11-24 15:21:17 -0300

    Tentando Resolver o Exercicio

--- Exercicio 50/exe50.js
+++ Exercicio 50/exe50.js
@@ -1,17 +1,20 @@
-let valorAleatorio = 5//Math.floor(Math.random() * 100) + 1
+let valorAleatorio = Math.floor(Math.random() * 100) + 1
 let tentativaUsuario = Number(prompt("Informe um número, e tente acertar com a máqunia ?"))
 
-
+if(tentativaUsuario != valorAleatorio){
     if (valorAleatorio > tentativaUsuario) {
         console.log("O numero sorteado foi " + valorAleatorio + " é MAIOR")
     }else{
         console.log("O numero sorteado foi " + valorAleatorio + " é MENOR")
-    }
-  
+    }  
+
+}
+
+for(let i = 0; i == valorAleatorio; i++){
+    console.log("O número de tentavias foi",tentativaUsuario);
+}
 
 
-
-console.log("A quantidade de tentativas" ,tentativaUsuario) 
 
 
 
