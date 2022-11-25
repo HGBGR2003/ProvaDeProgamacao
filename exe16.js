092cca7bc93736c8cc14a5056785a4497b971dc8
LucasAlvesG <117421570+LucasAlvesG@users.noreply.github.com>
2022-11-25 19:22:26 -0300

    Update exe16.js

--- exe16.js
+++ exe16.js
@@ -3,7 +3,6 @@ let num2 = 5
 function comparador(x, y){
     if(x>y){
         return (x + ' é o número maior e ' + y + " é o menor número")
-
     }
     else if(y>x){
         return (y + ' é o numero maior ' + x + " é o menor número")
