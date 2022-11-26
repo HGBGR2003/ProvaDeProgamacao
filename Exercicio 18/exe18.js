0d9b0995dcdd0b41411ec7bde958c8b9e583adba
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-11-26 07:44:57 -0300

    Exercicio Reorganizado

---
+++ Exercicio 18/exe18.js
@@ -0,0 +1,20 @@
+let codigo = Number(prompt("Informe o número e veja se bate com o que está de acordo 1, 2 e 3?"))
+
+switch(codigo){
+  
+    case 1:
+      console.log("Um")
+      break
+    
+    case 2:
+      console.log("Dois")
+      break
+    
+    case 3:
+      console.log("Três")
+      break
+    
+    default:
+          console.log("Códig Inválido")
+
+  }
