1422d985786841b4b4923b83513ac554d1ccbbc0
LucasAlvesG <lucasag_5@hotmail.com>
2022-12-02 11:18:49 -0300

    feitos

---
+++ Exercicio 23/exe23.js
@@ -0,0 +1,24 @@
+let adiçãoUsuario = confirm("deseja fazer uma adição ?")
+if(adiçãoUsuario == true){
+    let x = Number(prompt("digite o primeiro numero."))
+    let y = Number(prompt("digite o segundo numero."))
+    console.log((x + y)) 
+}
+let subtUsuario = confirm("deseja fazer uma subtração ?")
+if(subtUsuario == true){
+    let x = Number(prompt("digite o primeiro numero."))
+    let y = Number(prompt("digite o segundo numero."))
+    console.log((x - y)) 
+}
+let multiUsuario = confirm("deseja fazer uma multiplicação ?")
+if(multiUsuario == true){
+    let x = Number(prompt("digite o primeiro numero."))
+    let y = Number(prompt("digite o segundo numero."))
+    console.log((x * y)) 
+}
+let divUsuario = confirm("deseja fazer uma divisão ?")
+if(divUsuario == true){
+    let x = Number(prompt("digite o primeiro numero dividendo."))
+    let y = Number(prompt("digite o segundo numero divisor."))
+    console.log((x/y))
+}
