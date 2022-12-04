7035d719870a501fc782397d02c1042a6e1ebfdd
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-12-04 15:18:36 -0300

    Exercicio Feito com Sucesso

--- Exercicio 55/exe55.js
+++ Exercicio 55/exe55.js
@@ -1,13 +1,14 @@
- let fraseUsuario =  []
  let informandoValor = prompt("Informe um frase de até 4") //Teste de verificação
 
-while (informandoValor.length < 4) {
-    informandoValor = prompt("Digite de novo a frase corretamente")
-    fraseUsuario.push(informandoValor)
-}
+ let semEspacos
 
-if (fraseUsuario === " ") {
-    console.log(fraseUsuario)
-}
-/*let semEspacos = informandoValor.split(" ")
-console.log(semEspacos)*/
+ while (informandoValor.length < 4) {
+    informandoValor = prompt("Digite de novo a frase corretamente")
+
+ }
+
+ semEspacos = informandoValor.replace(/\s/g,'')
+ console.log(semEspacos)
+
+const ocorrencias = (informandoValor.match(/ /g) || []).length;
+console.log("Mostando a quantidade de espaços na frase: ",ocorrencias)
