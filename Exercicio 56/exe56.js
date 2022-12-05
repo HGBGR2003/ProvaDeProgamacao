6056ef0fdb6c0a208c675b983114ae6b6f22b1b2
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-12-05 18:16:05 -0300

    Exercicio em Andamento

---
+++ Exercicio 56/exe56.js
@@ -0,0 +1,14 @@
+let vetorPrincipal = []
+
+for(let i = 0; i < 6; i++){
+    let valorUsuario = Number(prompt("Informe um número positivo de 0 até 100"))
+    vetorPrincipal.push(valorUsuario)
+    console.log(vetorPrincipal)
+}
+
+if (valorUsuario %2 == 0) {
+    let cont = 0
+    cont++
+    console.log("A quantidade de números pares é",cont)
+}
+
