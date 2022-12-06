9aa90f97ae8a28c7a83be56eb228375684d49a2a
Dennis Rodrigues <dennisrodrigues1717@gmail.com>
2022-12-06 18:13:49 -0300

    Mudei o nome

--- exe.36.js
+++
@@ -1,8 +0,0 @@
-let nums = [244,29,10,11,666,14,2,4,61,64]
-function Ordenar(a,b) {
-    return a - b
-}
-nums.sort(Ordenar)
-console.log(nums)
-console.log(nums[0] + " é o menor número")
-console.log(nums[9]+ " é o maior número")
