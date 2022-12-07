095d8e4409b2df7a66307ec2afcf4a1c84c17d70
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-12-07 17:43:39 -0300

    Arrumados

--- Exercicio 52/exe52.js
+++ Exercicio 52/exe52.js
@@ -1,12 +1,16 @@
-let vetorPrincipal = [1,3,4,5,6,7,8,8,9,1,3,4,6,7,8,9,10,11,1,3,4,7,8,0]
-let somatorio = 2
+let vetorPrincipal = []
+let somatorio = 0
 let digitNumero
+let arrayDois = []
+let arrayControle = [1,3,4]
 do{
-    digitNumero = Number(prompt("Informe -1 para parar a execucão ?"))
-    if(vetorPrincipal.length == 1,3,4){
-        somatorio++
-        console.log("O numero de valores nesta ordem é" ,somatorio)
+    digitNumero = Number(prompt("Informe um número ou -1 para sair da execucão ?"))
+    vetorPrincipal.push(digitNumero)
+    arrayDois = vetorPrincipal.slice(-3)
+    
+    if(JSON.stringify(arrayDois) == JSON.stringify(arrayControle)){
+        somatorio++  
     }
 }while(digitNumero !=-1)
 
-
+console.log("O numero de valores nesta ordem é" ,somatorio)
