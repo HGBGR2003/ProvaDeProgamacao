264c4453b6396189a97e9df3032ebe7d0d01f50b
Dennis Rodrigues <dennisrodrigues1717@gmail.com>
2022-12-07 13:36:01 -0300

    Feito

---
+++ exe41.js
@@ -0,0 +1,19 @@
+let idade_nadador =  4
+if (idade_nadador <5) {
+    console.log ("Muito Novo")
+}
+else if(idade_nadador >=5 && idade_nadador <= 7 )  {
+    console.log("Infantil A")
+}
+else if (idade_nadador >=8 && idade_nadador <= 11) {
+    console.log("Infantil B")
+}
+else if (idade_nadador >=12 && idade_nadador <=13) {
+    console.log("Juvenil A")
+}
+else if (idade_nadador >=14 && idade_nadador <=17) {
+    console.log("Juvenil b")
+}
+else{
+    console.log("Adultos")
+}
