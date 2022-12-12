f8c0b532dccd57b9621211115ab8fc5e269e8e39
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-12-12 13:31:25 -0300

    Exercicio Arrumado e feito

--- Exercício 33/exe33.js
+++ Exercício 33/exe33.js
@@ -1,14 +1,17 @@
-let número_de_alunos = Number(prompt("Quantos alunos são?"))
-let matrícula_do_aluno
+let numero_de_alunos = Number(prompt("Quantos alunos são?"))
+let matricula_do_aluno
 let nota_do_aluno
 let soma = 0
-let notas = [ ] 
-for (let x = 0; x < número_de_alunos; x++) {
-   matrícula_do_aluno = Number(prompt("Qual a matrícula?"))
-    nota_do_aluno = Number(prompt("Qual foi a nota do aluno?:",matrícula_do_aluno))
+let notas = [] 
+for (let x = 0; x < numero_de_alunos; x++) {
+    matricula_do_aluno = Number(prompt("Qual a matrícula?"))
+    nota_do_aluno = Number(prompt("Qual foi a nota do aluno " + (matricula_do_aluno) + "?"))
+    notas.push(nota_do_aluno)
+    let matriculas = matricula_do_aluno
 }
-notas.push(nota_do_aluno)
-let matrículas = matrícula_do_aluno
-for (let i in notas){ soma += notas}
-let média = soma/número_de_alunos
-console.log(média)
+for(let x in notas){
+    soma += notas[x]
+}
+
+let media = soma/numero_de_alunos
+console.log(media.toFixed(1))
