ab0f774d59ae5dbf6d572b47a0a1d82962903c36
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-12-12 11:24:56 -0300

    Testa aí colega

--- Exercicio 15/mediaEscolar.js
+++ Exercicio 15/mediaEscolar.js
@@ -2,20 +2,23 @@ let n1 = Number(prompt("N1:"))
 let n2 = Number(prompt("N2:"))
 let n3 = Number(prompt("N3:"))
 let n4 = Number(prompt("N4:"))
+let notaRecuperacao
+let mediaAposRecuperacao 
 
-mediaEscolar = (n1+n2+n3+n4)/4
+let mediaEscolar = (n1+n2+n3+n4)/4
 
 if(mediaEscolar>=7){
+    let media = mediaEscolar
     console.log("ALUNO APROVADO COM MÉDIA :" , media )
 }else if(mediaEscolar<7){
-    let notaRecuperacao = (number(prompt("DIGITE A NOTA DA RECUPERACAO")))
+   notaRecuperacao = Number(prompt("DIGITE A NOTA DA RECUPERAÇÃO"))
 }
 
-mediaAposRecuperacao = (mediaEscolar+notaRecuperacao)/2
+mediaAposRecuperacao = (mediaEscolar + notaRecuperacao) / 2
 
 if(mediaAposRecuperacao>=7){
-    console.log("ALUNO APROVADO NA RECUPERAÇÃO COM MÉDIA:", mediaAposRecuperacao)
-}else if(mediaiAposRecuperacao<7){
+    console.log("ALUNO APROVADO NA RECUPERAÇÃO COM MÉDIA:", mediaAposRecuperacao.toFixed(0))
+}else if(mediaAposRecuperacao<7){
     console.log("ALUNO REPROVADO")
-    console.log("MÉDIA DO ALUNO:", mediaAposRecuperacao)
+    console.log("MÉDIA DO ALUNO:", mediaAposRecuperacao.toFixed(1))
 }
