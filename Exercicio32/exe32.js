7ef049eded6a6f79d876e364cb37e7afcf061336
Dennis Rodrigues <dennisrodrigues1717@gmail.com>
2022-12-12 17:25:56 -0300

    Não feito

---
+++ Exercicio32/exe32.js
@@ -0,0 +1,11 @@
+let numero_usuario = 3
+let impar = [ ]
+if(numero_usuario < 0 || numero_usuario > 10) {
+    console.log("O número está fora do padrão estabelecido")
+} 
+for(i = numero_usuario ; i =  40 + numero_usuario; i++) {
+    if(numero_usuario %2 == 1 ){
+       impar = i*i
+    } 
+}
+console.log(numero_usuario)
