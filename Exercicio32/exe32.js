33e86a862e3326f311bd483129f942583a9e9755
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-12-12 17:57:33 -0300

    Dennis tentei aqui e não deu nada, da uma olhada ai depois

--- Exercicio32/exe32.js
+++ Exercicio32/exe32.js
@@ -1,11 +1,14 @@
-let numero_usuario = 3
-let impar = [ ]
-if(numero_usuario < 0 || numero_usuario > 10) {
-    console.log("O número está fora do padrão estabelecido")
-} 
-for(i = numero_usuario ; i =  40 + numero_usuario; i++) {
-    if(numero_usuario %2 == 1 ){
-       impar = i*i
+let numero_usuario = 0
+let impar = 0
+
+do{
+    numero_usuario = Number(prompt("Informe um número maior que 0 e menor que 100"))
+}while(numero_usuario <= 0 || numero_usuario >= 10)
+   
+
+for(let i = numero_usuario ; i < 40 + numero_usuario; i++) {
+    if(i%2==1){
+       impar += i*i
     } 
 }
-console.log(numero_usuario)
+console.log("O resultado da soma dos 20 primeiros números é ", impar)
