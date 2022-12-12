c4d900b470d1e3168ded34ae4a8e5379eafdd3e6
Bella-devv <82674742+Bella-devv@users.noreply.github.com>
2022-12-12 08:53:04 -0300

    feito

---
+++ Exercicio 10/relacionamentoNumeros.js
@@ -0,0 +1,28 @@
+let a = prompt("Digite um numero:")
+let b = prompt("Digite outro numero")
+if(a!=b){
+    console.log(a, 'e', b, 'são diferentes')
+if(a>b){
+    console.log(a, "é maior que", b)
+}else{
+    console.log(a, "é menor que" , b)
+}if(b>a){
+    console.log(b,"é maior que", a)
+}else{
+    console.log(b,"é menor que", a)
+}
+if(a>=b){
+    console.log(a,">=", b)
+}else{
+    console.log(a, "<=" , b)
+}if(b>=a){
+    console.log(b, ">=" , a)
+}else{
+    console.log(b, "<=", a)
+}
+}else{
+    console.log ("são iguais")
+    console.log (a,">=", b)
+    console.log (b, ">=" , a)
+
+}
