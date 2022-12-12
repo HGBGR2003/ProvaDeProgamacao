b9e0ea5e73ff00f4972ba7864f028399071972e0
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-12-12 15:31:49 -0300

    Mandando para o outro

--- exe34.js
+++ exe34.js
@@ -1,8 +1,17 @@
 let numeros = [2,4,78,6,35,12,0]
+
 function Ordenar(a,b) {
     return a - b
 }
 numeros.sort(Ordenar)
-console.log(numeros)
-console.log(numeros[0] + " é o menor número")
-console.log(numeros[numeros.length]+ " é o maior número")
+
+const maxValue = numeros.reduce(function(prev,current){
+    return prev > current ? prev:current
+})
+
+const minValue = numeros.reduce(function(prev,current){
+    return prev < current ? prev:current
+})
+
+console.log(maxValue)
+console.log(minValue)
