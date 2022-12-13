1cea46958659b3b79e4c921d60431f15533d3f5d
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-12-13 13:45:13 -0300

    Consertando

--- Exercicio44/exe44.js
+++ Exercicio44/exe44.js
@@ -3,7 +3,7 @@ let encerrandoOuContinuando
 
 do{
     digiteNumero = Number(prompt(" Digite : 1 - Conversão de Graus Celsius Farenheit,    2 - Conversão Fahreint para celcius" +
-    " 3 - Peso ideal do Homem e     4 - Peso ideal da mulher"))
+    " 3 - Peso ideal do Homem e 4 - Peso ideal da mulher"))
 
     switch (digiteNumero) {
         case 1:
@@ -21,20 +21,23 @@ do{
         case 3:
             let alturaHomem = parseFloat(prompt("Informe a altura caso tenha escolhido Homem"))
             let pesoDOHomem = parseFloat(prompt("Qual seu peso?(M)"))
-            let pesoIdeal = (72.7 * alturaHomem) - 58;
-            if (pesoDOHomem > pesoIdeal){
-                console.log("vocé estpa acima do peso")
+            let imcHomem = pesoDOHomem / (alturaHomem * alturaHomem)
+            if (imcHomem > 25){
+                console.log("Você está acima do peso")
+            }else{
+                console.log("Você não está acima do peso")
             }
             break
         
         case 4:
            let alturaMulher = parseFloat(prompt("Informe a altura caso tenha escolhido Mulher"))
            let pesodamulher = parseFloat(prompt("Qual seu peso?(F)"))
-           pesoIdealF= (62.1 * alturaMulher) - 44.7
-           if (pesodamulher > pesoIdealF) {
-            console.log("você está acima do peso")
+           let imcMulher = pesodamulher / (alturaMulher * alturaMulher)
+           if (imcMulher > 25) {
+                console.log("Você está acima do peso")
+           }else{
+                console.log("Você não está acima do peso")
            }
-            
            break
     }
 
